f974c5d139bb0d8fcbb24e8c6f4ae5c1e89be80a
JD Patel <iamjdpatel@gmail.com>
2021-02-03 02:59:53 +0530

    day 2 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "JD";
+
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "JD";
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
