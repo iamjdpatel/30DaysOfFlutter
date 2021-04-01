c062de9481ebd207df21d6cbcdc372f40c27fba4
JD Patel <iamjdpatel@gmail.com>
2021-04-02 00:14:21 +0530

    day22 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -7,6 +7,63 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cart'),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: _CartList(),
+            ),
+          ),
+          Divider(),
+          _CartTotal()
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "\$9999",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              child: Text('Buy'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+            icon: Icon(Icons.remove_circle_outline), onPressed: () {}),
+        title: Text('item'),
+      ),
     );
   }
 }
